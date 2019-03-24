554eeed6eca16d5cc5e3ca57648af86dd3118bfb
maykol <maynoboa16@gmail.com>
2019-03-24 01:12:38 +0100

    ultimo

---
+++ mispublicaciones.php
@@ -0,0 +1,25 @@
+<?php
+require 'seguridad-global-2.php';
+require_once ('con1.php');	
+$mysqli = new mysqli($se, $us, $co, $bd);
+if ($mysqli->connect_errno) {
+    printf("Error en la conexion: %s\n", $mysqli->connect_error);
+    exit();
+}
+$resultado = $mysqli->query("SELECT DISTINCT id_pub,co_pub,co_pub,id_usu,fe_pub from pub,seg where id_usu=".$_SESSION['id_usu']." order by fe_pub desc");
+$numeroRegistros = $resultado->num_rows;
+if ($numeroRegistros) {
+    while ($fila = $resultado->fetch_assoc()) {
+        $resultado2 = $mysqli->query("SELECT no_usu ,img from usu where id_usu=".$fila['id_usu']."");
+        while ($fila2 = $resultado2->fetch_assoc() ) {
+            $nombre=$fila2['no_usu'];
+            $img=$fila2['img'];
+        }
+        echo "<div id='".$fila['id_pub']."' class='contenidoConcreto'><div class='nompub'><img src=" .$img." class='usu1 ber'><div class='maque'>".$nombre."</div></div><div class='comen'>".$fila['co_pub']."<div class='size'>publicado:".$fila['fe_pub']."</div></div></div>";
+
+    }
+}
+else{echo 'No hay publicaciones';}
+//  $resultado->free();
+//   $mysqli->close();
+?>
---
+++ motor.js
@@ -0,0 +1,20 @@
+var form = document.forms.namedItem("form1");
+form.addEventListener('submit', function(e) {
+ var oResultado1 = document.getElementById("resultado");
+ var oDatosFormulario1 = new FormData(document.forms.namedItem("form1"));
+ /* PRINCIPIO - Envío extra de datos, junto con la imagen */
+ oDatosFormulario1.append("dato1", "valor dato 1");
+ /* FIN - Envío extra de datos, junto con la imagen */
+ var oRequest1 = new XMLHttpRequest();
+ oRequest1.open("POST", "subir-fichero.php", true);
+ oRequest1.onload = function(oEvent) {
+ if (oRequest1.status == 200) {
+ oResultado1.innerHTML = oRequest1.responseText;
+ }
+ else {
+ oResultado1.innerHTML = "Se ha producido un error: " + oRequest1.status;
+ }
+ };
+ oRequest1.send(oDatosFormulario1);
+ e.preventDefault();
+}, false);
